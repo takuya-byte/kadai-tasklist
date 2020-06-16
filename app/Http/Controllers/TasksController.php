d0ba32c8d596ab851478e5f69fe39bbd533c1db4
takuya.okita <trainingdayokita@gmail.com>
2020-06-16 07:21:38 +0000

    add colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -39,7 +39,17 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        
+        // バリデーション
+        $this->validate($request,[
+            'status' =>'required|max:10',
+            'content'=>'required|max:255'
+            
+            ]);
+            
+            
         $task = new Task;
+        $task->status =$request->status;
         $task->content =$request->content;
         $task->save();
         
@@ -74,7 +84,16 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $this->validate($request,[
+            'status' =>'required|max:10',
+            'content' =>'required|max:255',
+            
+            ]);
+            
+            
         $task = Task::findOrFail($id);
+        $task->status =$request->status;
         $task->content = $request->content;
         $task->save();
 
