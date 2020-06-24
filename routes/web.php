3c7870d1aaf9180dd176e614d1882e66f155df60
takuya.okita <trainingdayokita@gmail.com>
2020-06-24 12:20:15 +0000

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('tasks','TasksController');
+Route::group(['middleware' => ['auth']],function(){
+    Route::resource('tasks','TasksController');
+});
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
